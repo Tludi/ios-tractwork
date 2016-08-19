d1dc5517e83f5e21430699d4be03dffecfd0d1de
Tludi <tludi00@gmail.com>
2016-08-18 17:02:55 -0700

    started base view. started database Object file for Realm

---
+++ tractwork/TimePunch.swift
@@ -0,0 +1,66 @@
+//
+//  TimePunch.swift
+//  Tractwork - Time Tracking
+//
+//  Created by manatee on 8/18/16.
+//  Copyright (c) 2016 diligentagility. All rights reserved.
+//
+//*** Objects ***//
+//***************//
+//  * Workday
+//  * TimePunch
+//  * Project
+
+
+import Foundation
+import RealmSwift
+
+
+class Workday: Object {
+  dynamic var id = ""
+  dynamic var dayDate = "date"
+  dynamic var project = "general work"
+  dynamic var totalHoursWorked = 8
+  dynamic var worker = "milo"
+  
+  override static func primaryKey() -> String? {
+    return "id"
+  }
+  
+  let timePunches = List<TimePunch>()
+  let projects = List<Project>()
+
+}
+
+
+class TimePunch: Object {
+  dynamic var id = ""
+  dynamic var punchTime = "time"
+  dynamic var status = false
+  
+  override static func primaryKey() -> String? {
+    return "id"
+  }
+  
+  
+  var timePunchWorkday = LinkingObjects(fromType: Workday.self, property: "timePunches")
+}
+
+
+
+class Project: Object {
+  dynamic var id = ""
+  dynamic var projectName = "general work"
+  dynamic var projectExternalID = "project ID"
+  dynamic var locationAddress = "1234 happy drive"
+  dynamic var locationCity = "Portland"
+  dynamic var locationState = "Oregon"
+  dynamic var locationZIP = "12345"
+  
+  override static func primaryKey() -> String? {
+    return "id"
+  }
+  
+  var workdayProject = LinkingObjects(fromType: Workday.self, property: "projects")
+  
+}
